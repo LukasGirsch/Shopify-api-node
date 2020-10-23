7f6702b99fd6ca5dacef79207fb99d296e825cab
Lukas Girsch <Lukas.Girsch@gmail.com>
2020-10-23 19:39:39 +0200

    events by order
    this is a test

--- resources/event.js
+++ resources/event.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const assign = require('lodash/assign');
-const omit = require('lodash/omit');
+const pick = require('lodash/pick');
 
-const base = require('../mixins/base');
+const baseChild = require('../mixins/base-child');
 
 /**
  * Creates an Event instance.
@@ -15,10 +15,27 @@ const base = require('../mixins/base');
 function Event(shopify) {
   this.shopify = shopify;
 
+  this.parentName = 'orders';
   this.name = 'events';
   this.key = 'event';
 }
 
-assign(Event.prototype, omit(base, ['create', 'delete', 'update']));
+assign(
+  Event.prototype,
+  pick(baseChild, ['buildUrl', 'create', 'delete', 'update'])
+);
+
+/**
+ * Gets a list of events for an order.
+ *
+ * @param {Number} orderId Order ID
+ * @param {Object} [params] Query parameters
+ * @return {Promise} Promise that resolves with the result
+ * @public
+ */
+Event.prototype.list = function list(orderID, params) {
+  const url = this.buildUrl(orderID, undefined, params);
+  return this.shopify.request(url, 'GET', this.name);
+};
 
 module.exports = Event;
